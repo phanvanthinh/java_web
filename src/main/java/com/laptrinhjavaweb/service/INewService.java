500e2a4082cba068b1cd630d3062286b16cc9bb4
phanvanthinh <phanvanthinh@users.noreply.github.com>
2020-06-15 20:52:41 +0700

    commit java_web

---
+++ src/main/java/com/laptrinhjavaweb/service/INewService.java
@@ -0,0 +1,14 @@
+package com.laptrinhjavaweb.service;
+
+import com.laptrinhjavaweb.dto.NewDTO;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface INewService {
+	List<NewDTO> findAll(Pageable pageable);
+	int getTotalItem();
+	NewDTO findById(long id);
+	NewDTO save(NewDTO dto);
+	void delete(long[] ids);
+}
